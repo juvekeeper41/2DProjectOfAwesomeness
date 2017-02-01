638bc263d3ebd29922812f5bcf2f23852ee62547
juvekeeper41 <josb6385@740s-pl-114.local>
2017-02-01 08:45:45 -0700

    Worked on establishing the 'Airplane' class.

--- src/grid/controller/GridController.java
+++ src/grid/controller/GridController.java
@@ -1,25 +1,35 @@
 package grid.controller;
 
 import javax.swing.JOptionPane;
+import grid.controller.GridRunner;
+import grid.view.*;
+import grid.model.*;
 
 public class GridController 
 {
 	private GridFrame appFrame;	
+	private Airplane [][] grid;
 	
 	public GridController()
 	{
 		//create model objects here!
-		
+		grid = new Airplane[6][5];
 		appFrame = new GridFrame(this);
 	}
 	
+	
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(appFrame, "Hey look, it's my 2D creation!");
 	}
 	
-	private void start()
+	public Airplane [][] getGrid()
 	{
-		JOptionPane.showMessageDialog(appFrame, "Hey look, it's my 2D creation!");
+		return grid;
+	}
+	
+	public GridFrame getFrame()
+	{
+		return appFrame;
 	}
 }
--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -2,6 +2,9 @@ package grid.view;
 
 import javax.swing.*;
 import grid.controller.GridController;
+import java.awt.Color;
+import javax.swing.table.*;
+import java.awt.Dimension;
 
 public class GridPanel extends JPanel
 {
@@ -17,21 +20,44 @@ public class GridPanel extends JPanel
 	
 	public GridPanel(GridController)
 	{
+		this.baseController = baseController;
+		this.baseLayout = springLayout()
 		
+	}
+	
+	private void setupTable()
+	{
+		DefaultTableModel data = new DefaultTableModel(baseController.getGrid(), new String []{"one", "two", "three", "four", "five"});
+		gridTable = new JTable();
+		gridTable.setModel(data);
+		gridPane = new JScrollPane();
+		gridPane.setViewportView(gridTable);
 	}
 	
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.setMinimumSize(new Dimension(700, 700));
+		this.setBackground(Color.GREEN);
+		this.add(gridPane);
+		this.add(columnField);
 	}
+	
 	
 	private void setupLayout()
 	{
 		
 	}
 	
+	public gridController getBaseController()
+	{
+		return baseController()
+	}
+	
 	private setupListeners()
 	{
-		
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		this.columnField = new JTextField(5);
 	}
 }
